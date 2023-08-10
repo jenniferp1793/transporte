5ec069e7bdf27cb582866c1764651fe283a5857b
jenniferp1793 <131495330+jenniferp1793@users.noreply.github.com>
2023-08-10 18:04:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-import express from "express";
-import 'dotenv/config';
-import clientes  from "./routes/clientes.js";
-import bus  from "./routes/bus.js";
-import ticket  from "./routes/ticket.js";
-import mongoose from "mongoose";
-import rutas from "./routes/rutas.js";
-
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-.then(()=> console.log("Connected to MongoDB"))
+import  express  from "express";
+import mongoose from 'mongoose';
+import "dotenv/config"
+import bus from "./routes/bus.js"
+import cliente from "./routes/clientes.js"
+import conductor from "./routes/conductor.js";
+import horario from "./routes/horario.js";
+import ruta from "./routes/ruta.js"
+import ticket from "./routes/ticket.js"
+import vendedor from "./routes/vendedores.js"
 
 const app = express()
-app.use(express.json())
-app.use(   "/api/clientes"   ,clientes);
-app.use(   "/api/bus"   ,bus);
-app.use(   "/api/ticket"   ,ticket);
-app.use(   "/api/ruta"   ,rutas);
+app.use(express.json()) 
 
+app.use("/api/bus", bus)
+app.use("/api/cliente", cliente)
+app.use("/api/conductor", conductor)
+app.use("/api/horario", horario)
+app.use("/api/ruta", ruta)
+app.use("/api/ticket", ticket)
+app.use("/api/vendedor", vendedor)
 
 app.listen(process.env.PORT,()=>{
-  console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
-  
+    mongoose.connect(process.env.mongoDB)
+    .then(() => console.log('Conectado!'));
+    console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
 })
